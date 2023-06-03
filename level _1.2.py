4f1adc8ea2eda784fc85d7d021a6ed89be4b8f37
MariaKotsur1 <133589487+MariaKotsur1@users.noreply.github.com>
2023-06-03 22:53:56 +0400

    level_1.2

---
+++ level _1.2.py
@@ -0,0 +1,22 @@
+my_favorite_songs = [
+    ['Waste a Moment', 3.03],
+    ['New Salvation', 4.02],
+    ['Staying\' Alive', 3.40],
+    ['Out of Touch', 3.03],
+    ['A Sorta Fairytale', 5.28],
+    ['Easy', 4.15],
+    ['Beautiful Day', 4.04],
+    ['Nowhere to Run', 2.58],
+    ['In This World', 4.02],
+]
+
+
+#random_songs = (my_favorite_songs[1], 
+#my_favorite_songs[4], 
+#my_favorite_songs[6])
+#print (random_songs)
+
+#music = random_songs
+
+music = ((my_favorite_songs[1] [1]) + (my_favorite_songs[4] [1]) + (my_favorite_songs[6] [1]))
+print (music)
